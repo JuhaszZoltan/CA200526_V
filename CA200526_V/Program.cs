80cea540ab93a321c5ca9afcbac13c57c8d62df4
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-05-26 16:07:23 +0200

    resort

--- CA200526_V/Program.cs
+++ CA200526_V/Program.cs
@@ -8,7 +8,7 @@ namespace CA200526_V
 {
     class Program
     {
-        static List<Merkozes> mLista;
+        static List<Merkozes> _mLista;
         static void Main()
         {
             F02();
@@ -19,51 +19,45 @@ namespace CA200526_V
             F07();
             Console.ReadKey();
         }
-
-        private static void F07()
+        private static void F02()
         {
-            Console.WriteLine("7. feladat:");
-            mLista.GroupBy(x => x.Helyszin)
-                .Where(x => x.Count() > 20)
-                .ToList()
-                .ForEach(s => Console.WriteLine($"\t{s.Key}: {s.Count()}"));
+            _mLista = new List<Merkozes>();
+            var sr = new StreamReader(@"..\..\res\eredmenyek.csv", Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream) _mLista.Add(new Merkozes(sr.ReadLine()));
+            sr.Close();
         }
-
-        private static void F06()
-        {
-            Console.WriteLine("6. feladat:");
-            mLista.Where(x => x.Idopont == new DateTime(2004, 11, 21)).ToList()
-                .ForEach(x => Console.WriteLine($"\t{x.HCsapat} - {x.ICsapat} ({x.HPont}:{x.IPont})"));
-        }
-
-        private static void F05()
-        {
-            Console.Write("5. feladat: ");
-            Console.WriteLine($"barcelonai csapat neve: {mLista.Select(x => x.HCsapat).First(x => x.Contains("Barcelona"))}");
-        }
-
-        private static void F04()
-        {
-            Console.Write("4. feladat: ");
-            Console.WriteLine($"Volt döntetlen? {(mLista.Any(x => x.IPont == x.HPont) ? "igen" : "nem")}");
-        }
-
         private static void F03()
         {
             Console.Write("3. feladat: ");
             Console.WriteLine($"Real Madrid: " +
-                $"Hazai: {mLista.Count(x => x.HCsapat == "Real Madrid")}," +
-                $" Idegen: {mLista.Count(x => x.ICsapat == "Real Madrid")}");
+                $"Hazai: {_mLista.Count(x => x.HCsapat == "Real Madrid")}, " +
+                $"Idegen: {_mLista.Count(x => x.ICsapat == "Real Madrid")}");
         }
-
-        private static void F02()
+        private static void F04()
         {
-            mLista = new List<Merkozes>();
-            var sr = new StreamReader(@"..\..\res\eredmenyek.csv", Encoding.UTF8);
-            sr.ReadLine();
-            while (!sr.EndOfStream)
-                mLista.Add(new Merkozes(sr.ReadLine()));
-            sr.Close();
+            Console.Write("4. feladat: ");
+            Console.WriteLine($"Volt döntetlen? " +
+                $"{(_mLista.Any(x => x.IPont == x.HPont) ? "igen" : "nem")}");
+        }
+        private static void F05()
+        {
+            Console.Write("5. feladat: ");
+            Console.WriteLine($"barcelonai csapat neve: " +
+                $"{_mLista.Select(x => x.HCsapat).First(x => x.Contains("Barcelona"))}");
+        }
+        private static void F06()
+        {
+            Console.WriteLine("6. feladat:");
+            _mLista.Where(x => x.Idopont == new DateTime(2004, 11, 21)).ToList()
+                .ForEach(x => Console.WriteLine($"\t{x.HCsapat} - {x.ICsapat} ({x.HPont}:{x.IPont})"));
+        }
+        private static void F07()
+        {
+            Console.WriteLine("7. feladat:");
+            _mLista.GroupBy(x => x.Helyszin)
+                .Where(x => x.Count() > 20).ToList()
+                .ForEach(s => Console.WriteLine($"\t{s.Key}: {s.Count()}"));
         }
     }
 }
